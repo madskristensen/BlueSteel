d0dc681915eb8eecccd6fccd253af888e841a61b
Mads Kristensen <madsk@microsoft.com>
2025-12-09 14:44:40 -0800

    Renamed extension [release]

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -9,7 +9,7 @@ namespace BlueSteel
     internal sealed partial class Vsix
     {
         public const string Id = "BlueSteel.e1e706e2-05d3-4da9-8754-652cd8ab65f4";
-        public const string Name = "Blue Steel Theme";
+        public const string Name = "Blue Steel Theme Pack";
         public const string Description = @"A theme pack for Visual Studio 2026 containing several light, dark, and solarized color themes";
         public const string Language = "en-US";
         public const string Version = "1.0.900";
